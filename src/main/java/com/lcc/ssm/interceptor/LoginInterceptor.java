0b98b10c0d8af3512236ba9921a118907ee58e86
liangchengcheng <1038127753@qq.com>
2017-06-09 07:20:48 +0800

    lccssm
    lccssm

---
+++ src/main/java/com/lcc/ssm/interceptor/LoginInterceptor.java
@@ -0,0 +1,30 @@
+package com.lcc.ssm.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by liangchengcheng on 2017/6/8.
+ */
+public class LoginInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        //拿到cookie,就是拿到了session的登录的状态
+        String cookie ;
+        return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+    }
+}
---
+++ src/main/java/com/lcc/ssm/utils/Constant.java
@@ -0,0 +1,8 @@
+package com.lcc.ssm.utils;
+
+/**
+ * Created by liangchengcheng on 2017/6/8.
+ */
+public class Constant {
+    public static final String INDEXURL_ALL ="F://ssm/All";
+}
